f62cb92a3f51b0a78583b73c67929f0f725fb795
davidliu <davidliu@deviange.net>
2020-07-20 15:34:52 -0700

    Update ReusableExample.kt

--- app/src/main/java/com/deviange/daggerintro/scopes/ReusableExample.kt
+++ app/src/main/java/com/deviange/daggerintro/scopes/ReusableExample.kt
@@ -10,6 +10,10 @@ import javax.inject.Provider
 class ReusableModule {
     var increment = 0
 
+    /**
+     * @Reusable is like a non-synchronized lazy; once provided, the same value
+     * will be provided every time, and the provide method won't be called again.
+     */
     @Reusable
     @Provides
     fun provideIncrement() = increment++
