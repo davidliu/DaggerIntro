ef6c2fbbec20eadf7005b71fd640537350e331e1
davidliu <davidliu@deviange.net>
2022-05-03 00:02:02 +0900

    Update BasicModuleExample.kt

--- app/src/main/java/com/deviange/daggerintro/basic/basic_module/BasicModuleExample.kt
+++ app/src/main/java/com/deviange/daggerintro/basic/basic_module/BasicModuleExample.kt
@@ -3,6 +3,7 @@ package com.deviange.daggerintro.basic.basic_module
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import javax.inject.Provider;
 
 class Car
 
@@ -15,4 +16,5 @@ class CarModule {
 @Component(modules = [CarModule::class])
 interface CarComponent {
     fun giveMeACar(): Car // Will query CarModule.providesCar() for the answer.
+    fun giveMeACarProvider(): Provider<Car> // Gives a Provider<Car> object that can do the same thing as CarComponent.giveMeACar()
 }
